c47167ee7d3a9d557de2fb98b347fda5e59abf69
Chris D'Angelo <cd2665@columbia.edu>
2013-01-06 01:27:44 -0500

    deleted unnecessary function stubs

--- FlickrFun/PhotosFromPlaceTableViewController.m
+++ FlickrFun/PhotosFromPlaceTableViewController.m
@@ -39,30 +39,4 @@
     });
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
-
-// using inherited method
-// - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-
 @end
--- FlickrFun/PhotosRecentTableViewController.m
+++ FlickrFun/PhotosRecentTableViewController.m
@@ -17,44 +17,10 @@
 
 @implementation PhotosRecentTableViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
 - (void)viewWillAppear:(BOOL)animated
 {
     self.photos = [[NSUserDefaults standardUserDefaults] objectForKey:RECENTS_KEY];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.identifier isEqualToString:@"ShowPhoto"]) {
-        NSDictionary *photo = [self.photos objectAtIndex:[self.tableView indexPathForCell:sender].row];
-        [segue.destinationViewController setPhoto:photo];
-    }
-    
-    
-}
-
-// using inherited method
-// - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 
 
 @end
